5a72d05bca2ef95859501db9222a0b4902446f65
edenlisk <nkumbuyenock@gmail.com>
2024-06-12 01:41:27 +0200

    Add helper functions (upload files with multer)

---
+++ backend/utils/helperFunctions.js
@@ -0,0 +1,51 @@
+import multer from "multer";
+import * as path from "path";
+import {GraphQLError} from 'graphql';
+import {expressjwt} from "express-jwt";
+
+export function appError(message, code) {
+    return new GraphQLError(message, { extensions: { code } });
+}
+
+
+const multerStorage = multer.diskStorage(
+    {
+        destination: function(req, file, cb) {
+            cb(null, `${__dirname}/../public/temp`);
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    }
+)
+
+const multerFilter = (req, file, cb) => {
+    const fileExtension = path.extname(file.originalname);
+    const allowedExtensions = ['.png', '.jpg', 'pdf', '.jpeg'];
+    if (allowedExtensions.includes(fileExtension)) {
+        cb(null, true)
+    } else {
+        cb(throw appError("Not a .jpg, .jpeg, .png or pdf file selected", 'INVALID_INPUT'), false);
+    }
+}
+
+export const upload = multer(
+    {
+        storage: multerStorage,
+        fileFilter: multerFilter
+    }
+)
+
+export function getContext({req}) {
+    console.log('[getContext]')
+    return {  }
+}
+
+export const authMiddleware = expressjwt(
+    {
+        secret: process.env.JWT_SECRET,
+        algorithms: ['HS256'],
+        credentialsRequired: false
+    }
+)
+
