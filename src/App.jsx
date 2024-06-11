8a4edcd717402a4d80835d2d65b4b1e8bb19de5a
ijosue16 <iradukundajosue16@gmail.com>
2024-06-11 18:21:43 +0200

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/Homepage'
+import Layout from './components/layout'
+import About from './pages/AboutPage'
+import Services from './pages/ServicesPage'
+import Team from './pages/TeamPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +13,12 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
+        <Route element={<Layout/>}>
         <Route path='/*' element={<HomePage/>}></Route>
+        <Route path='/about-us' element={<About/>}></Route>
+        <Route path='/services' element={<Services/>}></Route>
+        <Route path='/our-team' element={<Team/>}></Route>
+        </Route>
       </Routes>
       </BrowserRouter>
     </>
