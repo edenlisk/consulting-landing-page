096023aeb7ed74d84f946149c92ce4e784a170b8
Edenlisk <nkumbuyenock@gmail.com>
2024-06-13 23:20:36 +0200

    use graphql-upload middleware to support upload files.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 import { ApolloServer } from "@apollo/server";
+import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.mjs";
+// import {GraphQLUpload, graphqlUploadExpress, } from 'graphql-upload';
 import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
 import express from 'express';
 import { readFile } from 'node:fs/promises';
@@ -10,18 +12,18 @@ config();
 const PORT = process.env.PORT;
 const app = express();
 const typeDefs = await readFile('./schema/schema.graphql', 'utf8');
-
+app.use(graphqlUploadExpress());
 app.use(cors(), express.json());
 app.set('port', PORT);
 
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({typeDefs, resolvers, csrfPrevention: false});
 await connectDb();
 
 await server.start();
 app.use('/graphql', apolloMiddleware(server));
 
 app.listen({port: PORT}, () => {
-    console.log(`Server is listening on ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
     console.log(`Graphql server is listening on http://localhost:${PORT}/graphql`);
 })
 
