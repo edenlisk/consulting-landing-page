5f6c78aba296ad14e259b21bf944055e0312b1b4
edenlisk <nkumbuyenock@gmail.com>
2024-06-15 22:00:35 +0200

    Add multiple graphql queries.

--- frontend/src/api/graphql.js
+++ frontend/src/api/graphql.js
@@ -14,6 +14,7 @@ export function createApolloClient() {
     })
 }
 
+
 export const GET_SERVICES = gql`
     query fetchServices {
         services {
@@ -22,23 +23,63 @@ export const GET_SERVICES = gql`
         }
     }
 `
+// get one service
+const GET_SERVICE = gql`query Query($serviceId: ID!) {
+    service(serviceId: $serviceId) {
+        description
+        slug
+        title
+    }
+}`;
 
-// export const GET_TODOS =  gql`
-//     query GetTodos($userId: ID!) {
-//         todos(userId: $userId) {
-//             description
-//             date
-//             dueDate,
-//             isCompleted
-//         }
-//     }
-// `
-// const apolloClient = new ApolloClient({
-//     link: from([httpLink]),
-//     cache: new InMemoryCache(),
-//     defaultOptions: {
-//         query: {
-//             fetchPolicy: 'cache-first'
-//         }
-//     }
-// })
+
+
+// variables
+// {
+//     "serviceId": "666de43791421cd4d57fbccb"
+// }
+
+// get messages
+
+const GET_MESSAGES = gql`query GetMessages {
+    getMessages {
+        senderName
+        senderEmail
+    }
+}`
+
+// send message
+
+const SEND_MESSAGE = gql`mutation Mutation($input: MessageInput!) {
+    newMessage:sendMessage(input: $input) {
+        senderName
+    }
+}`
+
+// variables
+// {
+//     "input": {
+//     "senderEmail": "user1@gmail.com",
+//         "senderName": "test user1",
+//         "senderPhoneNumber": "+250786957549",
+//         "textMessage": "Hello my guy"
+// }
+// }
+
+const ADD_PHOTO_TO_GALLERY = gql`mutation AddPhotoToGallery($input: PhotoInput, $file: Upload) {
+    addPhotoToGallery(input: $input, file: $file) {
+        fileId
+        filePath
+        showInGallery
+    }
+}`;
+
+// variables
+// {
+//     "input": {
+//     "description": "Mine_site_visit",
+//         "showInGallery": true,
+//         "year": "2023"
+// },
+//     file: attach file
+// }
