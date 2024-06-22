5c101d1dd97fd38699201df5ae856b2943d83ff7
edenlisk <nkumbuyenock@gmail.com>
2024-06-22 23:55:49 +0200

    Add all graphql queries and mutations.

--- frontend/src/api/graphql.js
+++ frontend/src/api/graphql.js
@@ -14,6 +14,37 @@ export function createApolloClient() {
     })
 }
 
+// export const GET_SERVICES = gql`
+//     query getServices {
+//         services {
+//             description
+//             title
+//             image
+//         }
+//     }
+// `;
+
+// QUERIES
+export const GET_SERVICES = gql`
+    query fetchServices {
+        services {
+            title,
+            description
+            image {
+                filePath
+            }
+        }
+    }
+`;
+
+export const GET_SERVICE = gql`query Query($serviceId: ID!) {
+    service(serviceId: $serviceId) {
+        description
+        slug
+        title
+    }
+}`;
+
 export const GET_USERS = gql`
     query Users {
         team:users {
@@ -32,6 +63,23 @@ export const GET_USERS = gql`
         }
     }
 `;
+
+export const GALLERY = gql`
+    query gallery {
+        getGallery {
+            filePath,
+            description,
+            year
+        }
+    }
+`;
+
+export const GET_MESSAGES = gql`query GetMessages {
+    getMessages {
+        senderName
+        senderEmail
+    }
+}`
 
 export const GET_COMPANY_INFO = gql`
     query GetCompany {
@@ -61,73 +109,131 @@ export const GET_COMPANY_INFO = gql`
     }
 `;
 
-
-export const GET_SERVICES = gql`
-    query fetchServices {
-        services {
+export const GET_BLOGS = gql`
+    query getBlogs {
+        blogs:getBlogs {
             title,
+            content,
+            slug,
+            createdAt,
+            updatedAt
+        }
+    }
+`;
+
+export const BLOG = gql`
+    query blog($blogId: ID!) {
+        blog:getBlog(blogId: $blogId) {
+            content,
+            title,
+            slug,
+            createdAt,
+            updatedAt
+        }
+    }
+`;
+
+// MUTATIONS
+export const ADD_SERVICE = gql`
+    mutation addService($input: ServiceInput, $file: Upload) {
+        service:addService(input: $input, file: $file) {
+            title
+            description
+            image {
+                filePath
+            }
+        }
+    }
+`;
+
+export const DELETE_SERVICE = gql`
+    mutation deleteService($serviceId: ID!) {
+        service:deleteService(serviceId: $serviceId) {
+            title
+            description,
+            image {
+                filePath
+            }
+        }
+    }
+`;
+
+export const ADD_PHOTO_TO_GALLERY = gql`
+    mutation addToGallery($file: Upload) {
+        photo:addPhotoToGallery(file: $file) {
+            description,
+            filePath,
+            year
+        }
+    }
+`;
+
+export const REMOVE_PHOTO_FROM_GALLERY = gql`
+    mutation removePhoto($photoId: ID!) {
+        photo:removeFromGallery(photoId: $photoId) {
             description
         }
     }
-`
-// get one service
-export const GET_SERVICE = gql`query Query($serviceId: ID!) {
-    service(serviceId: $serviceId) {
-        description
-        slug
-        title
+`;
+
+export const SEND_MESSAGE = gql`
+    mutation Mutation($input: MessageInput!) {
+        newMessage:sendMessage(input: $input) {
+            senderName
+            senderName
+        }
     }
-}`;
+`;
 
-
-
-// variables
-// {
-//     "serviceId": "666de43791421cd4d57fbccb"
-// }
-
-// get messages
-
-export const GET_MESSAGES = gql`query GetMessages {
-    getMessages {
-        senderName
-        senderEmail
+export const ADD_USER = gql`
+    mutation addUser($input: UserInput, $file: Upload) {
+        user:addUser(input: $input, file: $file) {
+            background
+            position
+        }
     }
-}`
+`;
 
-// send message
-
-export const SEND_MESSAGE = gql`mutation Mutation($input: MessageInput!) {
-    newMessage:sendMessage(input: $input) {
-        senderName
-        senderName
+export const ADD_COMPANY_INFO = gql`
+    mutation addCompanyInfo($input: CompanyInput, $file: Upload) {
+        company:addCompanyInfo(input: $input, file: $file) {
+            name
+            logo {
+                filePath
+            }
+            address
+            socials
+        }
     }
-}`
+`;
 
-// variables
-// {
-//     "input": {
-//     "senderEmail": "user1@gmail.com",
-//         "senderName": "test user1",
-//         "senderPhoneNumber": "+250786957549",
-//         "textMessage": "Hello my guy"
-// }
-// }
-
-export const ADD_PHOTO_TO_GALLERY = gql`mutation AddPhotoToGallery($input: PhotoInput, $file: Upload) {
-    addPhotoToGallery(input: $input, file: $file) {
-        fileId
-        filePath
-        showInGallery
+export const UPDATE_COMPANY_INFO = gql`
+    mutation updateCompany($input: CompanyInput, $file:Upload) {
+        company:updateCompany(input: $input, file: $file) {
+            name
+            socials
+            aboutUs
+        }
     }
-}`;
+`;
 
-// variables
-// {
-//     "input": {
-//     "description": "Mine_site_visit",
-//         "showInGallery": true,
-//         "year": "2023"
-// },
-//     file: attach file
-// }
+export const ADD_BLOG = gql`
+    mutation addBlog($input: BlogInput, $file: Upload) {
+        blog: addBlog(input: $input, file: $file) {
+            title
+            slug
+            content
+        }
+    }
+`;
+
+export const UPDATE_BLOG = gql`
+    mutation updateBlog($input: BlogInput, $file: Upload, $blogId: ID!) {
+        blog:updateBlog(input: $input, file: $file, blogId: $blogId) {
+            title
+            content
+            slug
+        }
+    }
+`;
+
