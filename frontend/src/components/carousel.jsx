8cfdde7b06e7d0c5f656712945bb96c93845dc83
ijosue16 <iradukundajosue16@gmail.com>
2024-08-25 09:59:44 +0200

    update: reduced code repetition

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -1,32 +1,71 @@
-import React, { useState } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-import {FiArrowRight} from "react-icons/fi";
-import {
-  LazyLoadComponent,
-  LazyLoadImage,
-} from "react-lazy-load-image-component";
+import React, { useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import { FiArrowRight } from "react-icons/fi";
+import { LazyLoadComponent, LazyLoadImage } from "react-lazy-load-image-component";
 import { Blurhash } from "react-blurhash";
+import { useNavigate } from "react-router-dom";
 
 const Carousel = () => {
-    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const navigate = useNavigate();
 
-    // const [isLoaded, setLoaded] = useState(false);
-    // const [isLoadStarted, setLoadStarted] = useState(false);
-  
-    // const handleLoad = () => {
-    //   setLoaded(true);
-    // };
-  
-    // const handleLoadStarted = () => {
-    //   console.log("Started: ");
-    //   setLoadStarted(true);
-    // };
-    
+  // const [isLoaded, setLoaded] = useState(false);
+  // const [isLoadStarted, setLoadStarted] = useState(false);
+
+  // const handleLoad = () => {
+  //   setLoaded(true);
+  // };
+
+  // const handleLoadStarted = () => {
+  //   console.log("Started: ");
+  //   setLoadStarted(true);
+  // };
+
+  const CarouselInfo = ({ img, alt, link }) => {
     return (
-      <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-          {/*TOFIX: APRROACH USING LAZY LOAD  */}
+      <>
+        <div className="relative flex flex-col items-center justify-center w-full h-full ">
+          <img
+            src={`${img}`}
+            alt={`${alt}`}
+            className="absolute object-cover object-center w-full h-full"
+          />
+        </div>
+
+        <div className="absolute top-0 w-full px-4 py-24 space-y-6 font-semibold text-white h-fit lg:px-28">
+          <div className="flex items-center gap-3 py-1">
+            <span className="w-[70px] p-[1.8px] rounded-full bg-red-500"></span>
+            <p className="text-lg">Meet Rani Mining Company</p>
+          </div>
+          <p className="w-full py-1 text-5xl md:text-6xl md:w-3/4">
+            Elevate business operations with Mining Process
+          </p>
+          <p className="py-1 text-lg">
+            We know how to achieve the highest standards most productively
+          </p>
+          <div className="flex items-center gap-6 space-y-4">
+            <button
+              onClick={() => navigate(`/about-us`)}
+              type="button"
+              className="px-6 py-3 text-lg bg-orange-400 rounded-md "
+            >
+              Learn more
+            </button>
+            <button onClick={() => navigate(`${link}`)} className="flex items-center gap-2">
+              <p className="md:text-lg">All services</p>
+              <FiArrowRight className="text-xl text-orange-500 hover:text-white" />
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {/*TOFIX: APRROACH USING LAZY LOAD  */}
         {/* <section className="relative flex flex-col items-center justify-center embla__slide">
           <LazyLoadImage
           style={{margin:0,objectFit:'cover',objectPosition:'center',position:'absolute'}}
@@ -50,99 +89,37 @@ const Carousel = () => {
         // </LazyLoadComponent>
       )}
           </section> */}
-          {/* 1ST SLIDE */}
-          <section className="embla__slide bg-[url('https://moderndiplomacy.eu/wp-content/uploads/2023/12/business-office-1068x712.jpg')] bg-cover bg-center  text-white font-semibold">
-          <div className="w-full px-4 py-24 space-y-6 h-fit lg:px-28">
-                    <div className="flex items-center gap-3 py-1">
-                        <span className="w-[70px] p-[1.8px] rounded-full bg-red-500"></span>
-                        <p className="text-lg">Meet Rani Mining Company</p>
-                    </div>
-                    <p className="w-full py-1 text-5xl md:text-6xl md:w-3/4">
-                        Elevate business operations with Mining Process
-                    </p>
-                    <p className="py-1 text-lg">
-                        We know how to achieve the highest standards most productively
-                    </p>
-                    <div className="flex items-center gap-6 space-y-4">
-                        <button
-                            type="button"
-                            className="px-6 py-3 text-lg bg-orange-400 rounded-md "
-                        >
-                            Learn more
-                        </button>
-                        <div className="flex items-center gap-2">
-                            <p className="md:text-lg">All services</p>
-                            <FiArrowRight className="text-xl text-orange-500 hover:text-white"/>
-                        </div>
-                    </div>
-                </div>
-          </section>
-          {/* 2ND SLIDE */}
-          <section className="relative flex flex-col items-center justify-center embla__slide">
-            <div className='relative flex flex-col items-center justify-center w-full h-full '>
-            <img src="https://images.unsplash.com/photo-1510851896000-498520af2236?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8b2ZmaWNlJTIwbmlnaHR8ZW58MHx8MHx8fDA%3D" alt="" className='absolute object-cover object-center w-full h-full'/>
-            </div>
-           
-            <div className="absolute top-0 w-full px-4 py-24 space-y-6 font-semibold text-white h-fit lg:px-28">
-                    <div className="flex items-center gap-3 py-1">
-                        <span className="w-[70px] p-[1.8px] rounded-full bg-red-500"></span>
-                        <p className="text-lg">Meet Rani Mining Company</p>
-                    </div>
-                    <p className="w-full py-1 text-5xl md:text-6xl md:w-3/4">
-                        Elevate business operations with Mining Process
-                    </p>
-                    <p className="py-1 text-lg">
-                        We know how to achieve the highest standards most productively
-                    </p>
-                    <div className="flex items-center gap-6 space-y-4">
-                        <button
-                            type="button"
-                            className="px-6 py-3 text-lg bg-orange-400 rounded-md "
-                        >
-                            Learn more
-                        </button>
-                        <div className="flex items-center gap-2">
-                            <p className="md:text-lg">All services</p>
-                            <FiArrowRight className="text-xl text-orange-500 hover:text-white"/>
-                        </div>
-                    </div>
-                </div>
-          </section>
-          {/* 3RD SLIDE */}
-          <section className="relative flex flex-col items-center justify-center embla__slide">
-            <div className='relative flex flex-col items-center justify-center w-full h-full '>
-            <img src="https://mineralseducationcoalition.org/wp-content/uploads/miner3.jpg" alt="" className='absolute object-cover object-center w-full h-full'/>
-            </div>
-           
-            <div className="absolute top-0 w-full px-4 py-24 space-y-6 font-semibold text-white h-fit lg:px-28">
-                    <div className="flex items-center gap-3 py-1">
-                        <span className="w-[70px] p-[1.8px] rounded-full bg-red-500"></span>
-                        <p className="text-lg">Meet Rani Mining Company</p>
-                    </div>
-                    <p className="w-full py-1 text-5xl md:text-6xl md:w-3/4">
-                        Elevate business operations with Mining Process
-                    </p>
-                    <p className="py-1 text-lg">
-                        We know how to achieve the highest standards most productively
-                    </p>
-                    <div className="flex items-center gap-6 space-y-4">
-                        <button
-                            type="button"
-                            className="px-6 py-3 text-lg bg-orange-400 rounded-md "
-                        >
-                            Learn more
-                        </button>
-                        <div className="flex items-center gap-2">
-                            <p className="md:text-lg">All services</p>
-                            <FiArrowRight className="text-xl text-orange-500 hover:text-white"/>
-                        </div>
-                    </div>
-                </div>
-          </section>
-
-        </div>
+        {/* 1ST SLIDE  */}
+        <section className="relative flex flex-col items-center justify-center embla__slide">
+          <CarouselInfo
+            img={
+              "https://moderndiplomacy.eu/wp-content/uploads/2023/12/business-office-1068x712.jpg"
+            }
+            alt={"people-in-office"}
+            link={"/services"}
+          />
+        </section>
+        {/* 2ND SLIDE */}
+        <section className="relative flex flex-col items-center justify-center embla__slide">
+          <CarouselInfo
+            img={
+              "https://images.unsplash.com/photo-1510851896000-498520af2236?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8b2ZmaWNlJTIwbmlnaHR8ZW58MHx8MHx8fDA%3D"
+            }
+            alt={"office"}
+            link={"/services"}
+          />
+        </section>
+        {/* 3RD SLIDE */}
+        <section className="relative flex flex-col items-center justify-center embla__slide">
+          <CarouselInfo
+            img={"https://mineralseducationcoalition.org/wp-content/uploads/miner3.jpg"}
+            alt={"mining"}
+            link={"/services"}
+          />
+        </section>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
