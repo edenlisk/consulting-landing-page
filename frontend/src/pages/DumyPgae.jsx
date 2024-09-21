64882865dfd5c3a269ade6e1145e0f14875ff55a
edenlisk <nkumbuyenock@gmail.com>
2024-09-21 23:49:55 +0200

    Remove files

--- frontend/src/pages/DumyPgae.jsx
+++
@@ -1,8 +0,0 @@
-const Dummy=()=>{
-return(
-
-    <div className="w-full h-full bg-green-400"></div>
-)
-
-}
-export default Dummy;
--- frontend/src/pages/admin-layout/states/postsReducer.jsx
+++
@@ -1,9 +0,0 @@
-
-const postsReducer=(state,action)=>{
-
-    switch(action.type){
-
-        case '':
-            
-    }
-};
